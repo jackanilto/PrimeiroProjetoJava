ec9e06d4e4fa6b006897c78a8a019ff202518454
Janilto Oliveira <55307672+jackanilto@users.noreply.github.com>
2024-06-30 00:38:06 -0300

    Initial commit

---
+++ src/main/java/PrimeiraClasse.java
@@ -0,0 +1,72 @@
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.println("O programa está rodando");
+
+        System.out.println("==============================================================");
+
+        //Variaveis
+        // Tipo nome = valor;
+        // Tipo nome;
+        // Tipo Inteiro 1, 2, 3 ,4 ,5
+        int valor = 0;
+        // Tipo Double ou Float 1.5, 2.8, 2.0
+        double total = 2890.00;
+        float total2 = 2900.5f;
+        // Tipo Boolean true ou false
+        boolean autorizado  = true;
+        boolean autorizado2 = false;
+
+
+        int caixadeInteiro = 0;
+
+
+        System.out.println("==============================================================");
+
+
+        String nome = "Janilto Oliveira"; //criei uma variavel NOME e atribui o valor Janilto Oliveira a ela
+        System.out.println(nome);         // Printei na tela o valor da variavel nome
+
+        String codigo = "asd 43343#$# jdjeur3435$$";
+        System.out.println(codigo);
+
+
+        System.out.println("==============================================================");
+
+
+        //Operadores + - * /
+        int valorinteiro = 10 ;           // Declara a variavel com nome inteiro e atribui o valor 10 a ela
+        valorinteiro = valorinteiro + 25; //a variavel inteiro recebe o valor +25
+        System.out.println(valorinteiro);
+
+
+        System.out.println("Teste Operadores==============================================");
+
+        //Fazendo teste e brincando
+        int valorTotal;
+        int valor1 = 15;
+        int valor2 = 10;
+        valorTotal = valor1 + valor2;
+        System.out.println("O valor Total é: " + valorTotal);
+
+
+        System.out.println("Teste Operadores==============================================");
+
+        int valinteiro = 10 ;           // Declara a variavel com nome inteiro e atribui o valor 10 a ela
+        valinteiro = valinteiro - 5; //a variavel inteiro recebe o valor -5
+        System.out.println("O valor é :" + valinteiro);
+
+
+
+        System.out.println("Teste Operadores==============================================");
+
+        // modulo ou mod pega o resto da divisão
+        double numeroDoble = 10;
+        numeroDoble = numeroDoble / 9 %10; //para pegar o resto da divisão acrecenta o simbolo % desta forma %10
+
+        System.out.println(numeroDoble);
+
+    }
+
+}
+
---
+++ src/main/java/codicionais_e_loops.java
@@ -0,0 +1,46 @@
+
+public class codicionais_e_loops {
+
+    public static void main(String[] args) {
+
+        // if else
+        //Controla o fluxo do programa  exemplo:
+
+        if(true){   // true o programa printa Valor 1 e se false ele printa Valor 2
+            System.out.println("1");
+        } else {
+            System.out.println("2");
+        }
+
+
+        System.out.println("======== Operadores Lógicos ========");
+
+        // Utilizamos operadores Lógicos
+        //> Maior
+        //< Menor
+        //>= Maior ou igual
+        //<= Menor ou Igual
+        //== Estritamente Igual
+        //!= Estritamente Diferente
+
+        if(1 >= 2){
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        // quando comparamos if(1 <= 2) o resultado será Verdadeiro pois o valor uma das comprações é verdadeira
+        // entao se uma for verdadeira o resultado será verdadeiro
+        // portanto se fizermos if(1 >= 2)  o resultado será Falso por que as duas comparações sao Falsas
+
+        // for
+
+        //while
+
+        // do while
+
+        // Switch case
+
+    }
+
+}
